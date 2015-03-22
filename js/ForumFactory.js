00d911b7c8f6b72bd1659947ab74170f5dc3d1a7
Mattias <mattias.linder87@gmail.com>
2015-03-22 21:37:38 +0100

    took away switch case

--- js/ForumFactory.js
+++ js/ForumFactory.js
@@ -3,29 +3,11 @@ angular.module('Measures')
 	var dbUrl = "http://localhost:8000/GameDev/db/db.php";
 	return {
 		getForum: function(forum, subject) {
-			console.log("....." + dbUrl + "?forum=" + forum + "&subforum=" + subject + ".....");
 			return 	$.get(dbUrl + "?forum=" + forum + "&subforum=" + subject);
 				
 		},
 		getSubjects: function(choice) {
-			console.log('choice: ', choice);
-			switch(choice) {
-				case 'Other':
-					return 	$.get(dbUrl + "?subject=other");
-					break;
-				case 'Programming':
-					return 	$.get(dbUrl + "?subject=programming");
-					break;
-				case 'Requirements':
-					return 	$.get(dbUrl + "?subject=requirements");
-					break;
-				case 'Gameideas':
-					return 	$.get(dbUrl + "?subject=gameideas");
-					break;			
-				default:
-					console.log("Wrong argument to FACTORY in getsubjects");
-					break;
-			}
+			return 	$.get(dbUrl + "?subject="+ choice);
 		},
 		getLatest: function() {
 			return $.get(dbUrl + "?latest=latestreply");
